9c555139863e9cbeb2b359cad7823dd8d28e1ee1
Daye Song <songdaye66@gmail.com>
2021-01-29 14:52:36 +0900

    [송다예] spring test

---
+++ src/main/java/study/team/abu/model/Coupon.java
@@ -0,0 +1,14 @@
+package study.team.abu.model;
+
+import lombok.Data;
+
+@Data
+public class Coupon {
+	private int coupno;
+	private String coup_name;
+	private String coup_condition;
+	private int coup_price;
+	private String reg_date;
+	private String exp_date;
+	private int memberno;
+}
