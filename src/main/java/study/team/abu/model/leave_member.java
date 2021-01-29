f7b6cc25ac91096abc90819915b0a8ea0266a0b3
namyunjae <dodovv107@gmail.com>
2021-01-29 15:39:56 +0900

    [남윤재] 스프링테스트

---
+++ src/main/java/study/team/abu/model/leave_member.java
@@ -0,0 +1,14 @@
+package study.team.abu.model;
+
+import lombok.Data;
+
+@Data
+public class leave_member {
+	private int leav_mem;
+	private String id;
+	private String name;
+	private String email;
+	private String phone;
+	private String reason;
+	private String leave_date;
+}
