da6005d788dbc4b49b6c46727bdeda4a04b8e065
kim-sun-a <gina305496@gmail.com>
2021-02-23 18:56:28 +0900

    [선아] 웹헬퍼 한글 지원 수정

--- src/main/java/study/team/abuhae/interceptor/AppInterceptor.java
+++ src/main/java/study/team/abuhae/interceptor/AppInterceptor.java
@@ -4,24 +4,28 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.extern.slf4j.Slf4j;
+import study.team.abuhae.helper.WebHelper;
 import study.team.abuhae.model.Member;
 import study.team.abuhae.model.Mom_info;
 import study.team.abuhae.model.Sitter_info;
 
 @Slf4j
 public class AppInterceptor implements HandlerInterceptor {
-	
+	@Autowired
+    WebHelper webHelper;
 	@Value("#{servletContext.contextPath}")
 	String contextPath;
 	
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		webHelper.init(request, response);
 		//session 객체 가져오기
 	 	HttpSession session = request.getSession();
 	 	//login 처리 담당하는 사용자 정보
